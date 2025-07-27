a798b741466be2d5324f66cca176eb537b75e309
Sean McBroom <sean.mcbroom@outlook.com>
2025-07-26 22:14:54 -0500

    refactor: update types

--- src/models/entry.ts
+++ src/models/entry.ts
@@ -1,7 +1,7 @@
 import Sense from "./sense";
 
 export default interface Entry {
-  ent_seq: number;
+  ent_seq: number | undefined; // Undefined until parsed, must be handled accordingly
   kanji: string[];
   kana: string[];
   senses: Sense[];
--- src/models/sense.ts
+++ src/models/sense.ts
@@ -1,7 +1,7 @@
 export default interface Sense {
+  ent_seq: number; // link to the entry
   glosses: string[];
   pos: string[];
   misc?: string[];
   field?: string[];
 }
-
