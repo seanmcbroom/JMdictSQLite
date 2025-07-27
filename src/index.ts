1efba7f89c64da1a58fe0eaefe01b829a614edb3
Sean McBroom <sean.mcbroom@outlook.com>
2025-07-26 23:13:34 -0500

    fix: use path.resolve for files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 import { createParser } from '@/parser/parser.js';
 import { JmdictDatabase } from '@/db/index.js';
 import EntityReplace from '@/parser/entityReplace.js';
@@ -10,23 +11,29 @@ const dbParser = createParser(db);
 
 const artifact = new DefaultArtifactClient();
 
+const dbPath = path.resolve('./data/jmdict.db');
+console.log(dbPath);
+
+
 fs.createReadStream('./data/jmdict.xml', { encoding: 'utf8' })
   .pipe(new EntityReplace()) // Handles entity replacements
   .pipe(dbParser) // Parsing stream
   .on('end', () => {
     console.log(`✅ Done parsing XML. Time elapsed: ${((Date.now() - startTime) / 1000).toFixed(2)}s`);
-    
+
     db.close();
 
     const now = new Date();
     const dateStr = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}`;
-    artifact.uploadArtifact(`JMDictSQLite-${dateStr}`, ['./data/jmdict.db'], './', {
-      retentionDays: 7
-    })
-      .then(() => {
-        console.log('📦 Artifact uploaded successfully.');
-      })
-      .catch((err) => {
-        console.error('❌ Failed to upload artifact:', err);
-      });
+
+    artifact.uploadArtifact(
+      `JMDictSQLite-${dateStr}`,
+      [path.resolve('./data/jmdict.db')],
+      path.resolve('./data'),
+      { retentionDays: 7 }
+    ).then(() => {
+      console.log('📦 Artifact uploaded successfully.');
+    }).catch((err) => {
+      console.error('❌ Failed to upload artifact:', err);
+    });
   });
