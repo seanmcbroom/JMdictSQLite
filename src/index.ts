4eb199928652c8c4369fad36e26e6cf515f3119e
Sean McBroom <sean.mcbroom@outlook.com>
2025-07-28 02:38:13 -0500

    breaking change

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { JmdictProcessor } from '@/app.js';
 
 const xmlPath = path.resolve('./data/jmdict.xml');
-const outPath = path.resolve('./data/jmdict.sqlite');
+outPath = path.resolve('./data/jmdict.sqlite');
 const jmdictProcessor = new JmdictProcessor(xmlPath, outPath);
 
 jmdictProcessor.process();
