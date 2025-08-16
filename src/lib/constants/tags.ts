6d54f33e2a706e81c7e71c39dccbb4c0a3b7731b
sean <sean.mcbroom@outlook.com>
2025-08-16 16:05:24 -0500

    fix: broken link

--- src/lib/constants/tags.ts
+++ src/lib/constants/tags.ts
@@ -1,7 +1,7 @@
 /**
  * Tag categories used in JMdict-based parsing.
  *
- * @see https://www.edrdg.org/jmwsgi/edhelp.py?svc=JMdict#kwabbr
+ * @see https://www.edrdg.org/jmwsgi/edhelp.py?svc=jmdict#kwabbr
  */
 export const tags = {
   pos: [
