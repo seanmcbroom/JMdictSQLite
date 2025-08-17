18e245f748f0697fdfaf6f3fadc4dc1b2b0a1b37
sean <sean.mcbroom@outlook.com>
2025-08-17 11:32:29 -0500

    fix: rename inputs/outputs

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 import { JMdictProcessor } from '@/lib/processor/index.js';
 
-const xmlPath = path.resolve('./data/JMdict.xml');
-const outPath = path.resolve('./data/JMdict.sqlite');
+const xmlPath = path.resolve('./data/jmdict.xml');
+const outPath = path.resolve('./data/jmdict.sqlite');
 
 new JMdictProcessor(xmlPath, outPath).process();
