60b3645e29b55923b65c1a40a5fe1d362c652284
Sean McBroom <sean.mcbroom@outlook.com>
2025-12-18 17:27:14 -0600

    refactor: move edrdg ftp archive download functionity for future use

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import path from 'node:path';
 
 import { JMdictProcessor } from '@/lib/processor/index.js';
+import { downloadJMdict } from '@/lib/util/download-edrdg-archive.js';
 
-const xmlPath = path.resolve('./data/jmdict.xml');
+const jmdictPath = await downloadJMdict();
+
+const xmlPath = path.resolve(jmdictPath);
 const outPath = path.resolve('./data/jmdict.sqlite');
 
 new JMdictProcessor(xmlPath, outPath).process();
---
+++ src/lib/util/download-edrdg-archive.ts
@@ -0,0 +1,73 @@
+import * as ftp from 'basic-ftp';
+import fs from 'fs';
+import path from 'path';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import * as zlib from 'zlib';
+
+const pipe = promisify(pipeline);
+
+/**
+ * Downloads and decompresses a specified archive file (.gz) from the FTP server.
+ * @param filename File name without the extension
+ * @returns Path to the decompressed XML file, e.g., 'data/JMdict_e.xml'
+ */
+async function downloadEDRDGArchive(filename: string): Promise<string> {
+  const ftpUrl = 'ftp.edrdg.org';
+  const ftpPath = `/pub/Nihongo/${filename}.gz`;
+  const localGzPath = path.join('data', `${filename}.gz`);
+  const localXmlPath = path.join('data', `${filename}.xml`);
+
+  // Ensure data directory exists
+  fs.mkdirSync('data', { recursive: true });
+
+  const client = new ftp.Client();
+
+  /* Check if the XML file already exists,
+   if it does, skip download and decompression. */
+  if (fs.existsSync(localXmlPath)) {
+    console.log(`${filename} already exists. Skipping download.`);
+
+    return localXmlPath;
+  }
+
+  // Download the .gz file from the FTP server
+  try {
+    await client.access({
+      host: ftpUrl,
+      secure: false,
+    });
+
+    await client.downloadTo(localGzPath, ftpPath);
+  } catch (err) {
+    console.error('FTP download failed:', err);
+    process.exit(1);
+  } finally {
+    client.close();
+  }
+
+  // Decompress the .gz file to .xml
+  try {
+    const fileContents = fs.createReadStream(localGzPath);
+    const writeStream = fs.createWriteStream(localXmlPath);
+    const unzip = zlib.createGunzip();
+
+    await pipe(fileContents, unzip, writeStream);
+  } catch (err) {
+    console.error('Decompression failed:', err);
+    process.exit(1);
+  }
+
+  // Delete the .gz file after decompression
+  fs.unlinkSync(localGzPath);
+
+  console.log(`Downloaded ${filename} to ${localXmlPath}`);
+
+  return localXmlPath;
+}
+
+const downloadJMdict = () => downloadEDRDGArchive('JMdict_e');
+const downloadJMnedict = () => downloadEDRDGArchive('JMnedict_e');
+const downloadKanjidic = () => downloadEDRDGArchive('kanjidic');
+
+export { downloadJMdict, downloadJMnedict, downloadKanjidic };
